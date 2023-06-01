9cef5a14216ced68f538b5d2c1b077a926a62f5e
Eduardo Gonzalez <wm.eddie@gmail.com>
2023-06-01 11:50:13 +0900

    Add conversations for ChatComponent. and remove state.

--- openai_components.py
+++ openai_components.py
@@ -62,32 +62,36 @@ class OpenAIGenerate(Component):
 @xai_component
 class OpenAIChat(Component):
     model_name: InCompArg[str]
-    system_prompt: InCompArg[str]
-    user_prompt: InCompArg[str]
+    system_prompt: InArg[str]
+    user_prompt: InArg[str]
+    conversation: InArg[list]
     max_tokens: InArg[int]
     temperature: InArg[float]
     count: InArg[int]
     completion: OutArg[str]
-    
-    messages: list
-    
+    out_conversation: OutArg[list]
+        
     def __init__(self):
         super().__init__()
-        self.messages = []
-
+        
     def execute(self, ctx) -> None:
-        if not self.messages:
-            self.messages = [
+        messages = None
+        if self.conversation.value is not None:
+            messages = self.conversation.value
+        elif self.system_prompt.value is not None:
+            messages = [
                 {"role": "system", "content": self.system_prompt.value},
                 {"role": "user", "content": self.user_prompt.value}
             ]
         else:
-            self.messages.append({"role": "user", "content": self.user_prompt.value })
+            messages.append({"role": "user", "content": self.user_prompt.value })
         
+        if messages is None:
+            raise Exception("At least one prompt is required")
         
         result = openai.ChatCompletion.create(
             model=self.model_name.value,
-            messages=self.messages,
+            messages=messages,
             max_tokens=self.max_tokens.value if self.max_tokens.value is not None else 16,
             temperature=self.temperature.value if self.temperature.value is not None else 1,
             n=self.count.value if self.count.value is not None else 1
@@ -96,8 +100,9 @@ class OpenAIChat(Component):
 
         if self.count.value is None or self.count.value == 1:
             response = result['choices'][0]['message']
-            self.messages.append(response)
+            messages.append(response)
         self.completion.value = result['choices'][0]['message']['content']
+        self.out_conversation.value = messages
 
 
 @xai_component
@@ -199,3 +204,70 @@ class TakeNthElement(Component):
     
     def execute(self, ctx) -> None:
         self.out.value = self.values.value[self.index.value]
+
+
+@xai_component
+class FormatConversation(Component):
+    prev_conversation: InArg[list]
+    system_prompt: InArg[str]
+    user_prompt: InArg[str]
+    faux_assistant_prompt: InArg[str]
+    input_prompt: InArg[str]
+    input_is_system: InArg[bool]
+    args: InArg[dict]
+    out_messages: OutArg[list]
+    
+    def execute(self, ctx) -> None:
+        conversation = [] if self.prev_conversation.value is None else self.prev_conversation.value
+        format_args = {} if self.args.value is None else self.args.value
+        
+        
+        if self.system_prompt.value is not None:
+            conversation.append(self.make_msg('system', self.system_prompt.value.format(**format_args)))
+            
+        if self.user_prompt.value is not None:
+            conversation.append(self.make_msg('user', self.user_prompt.value.format(**format_args)))
+        
+        if self.faux_assistant_prompt.value is not None:
+            conversation.append(self.make_msg('assistant', self.faux_assistant_prompt.value.format(**format_args)))
+
+        if self.input_prompt.value is not None:
+            conversation.append(self.make_msg('system' if self.input_is_system.value else 'user', self.input_prompt.value.format(**format_args)))
+        
+        self.out_messages.value = conversation
+        
+        
+    def make_msg(self, role, msg) -> dict:
+        return { 'role': role, 'content': msg }
+
+@xai_component
+class AppendConversationResponse(Component):
+    conversation: InCompArg[list]
+    assistant_message: InCompArg[str]
+    out_conversation: OutArg[list]
+    
+    def execute(self, ctx) -> None:
+        ret = self.conversation.value + [{ 'role': 'assistant', 'content': self.assistant_message.value}]
+        self.out_conversation.value = ret
+
+        
+@xai_component
+class JoinConversations(Component):
+    conversation_1: InArg[list]
+    conversation_2: InArg[list]
+    conversation_3: InArg[list]
+    
+    out_conversation: OutArg[list]
+    
+    def execute(self, ctx) -> None:
+        ret = []
+        
+        if self.conversation_1.value is not None:
+            ret = ret + self.conversation_1.value
+        if self.conversation_2.value is not None:
+            ret = ret + self.conversation_2.value
+        if self.conversation_3.value is not None:
+            ret = ret + self.conversation_3.value
+            
+        self.out_conversation.value = ret
+
